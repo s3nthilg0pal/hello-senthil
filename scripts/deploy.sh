2bbb392e0c9d2d41460804c094b75af7ed66d2f0
senthil <sksanthoshg94@gmail.com>
2025-07-17 22:54:41 +1200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,12 @@ set -e
 echo "Starting deployment to Azure Static Web Apps..."
 
 echo "Using token: ${AZURE_STATIC_WEB_APPS_TOKEN:0:5}******"  # For debug (optional)
+
+export NVS_HOME="$HOME/.nvs"
+source "$NVS_HOME/nvs.sh"
+
 nvs use 24
+
 swa deploy ./public \
   --deployment-token "$AZURE_STATIC_WEB_APPS_TOKEN" \
   --env production
