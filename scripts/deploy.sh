cafcf53473a8e21d0e8f46a954621279af63b5a0
senthil <sksanthoshg94@gmail.com>
2025-07-17 22:53:46 +1200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Starting deployment to Azure Static Web Apps..."
 
 echo "Using token: ${AZURE_STATIC_WEB_APPS_TOKEN:0:5}******"  # For debug (optional)
-
+nvs use 24
 swa deploy ./public \
   --deployment-token "$AZURE_STATIC_WEB_APPS_TOKEN" \
   --env production
