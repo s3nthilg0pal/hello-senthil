62e86074c9f08f3a708f2496bc4d0ac5a6649323
senthil <sksanthoshg94@gmail.com>
2025-09-04 11:45:27 +1200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,31 +3,24 @@
 set -e
 echo "Starting deployment to Azure Static Web Apps..."
 
-# Install nvm
 export NVM_DIR="$HOME/.nvm"
 mkdir -p "$NVM_DIR"
 export PROFILE="/dev/null"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-# Load nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install and use Node.js 24
 nvm install 24
 nvm use 24
 
-# Confirm Node version
 node -v
 npm -v
 
-# Install Azure Static Web Apps CLI if not already installed
 npm install -g @azure/static-web-apps-cli
 
-# Print config
 swa --print-config
 
-# Deploy
 swa deploy ./public \
   --deployment-token "$AZURE_STATIC_WEB_APPS_TOKEN" \
   --env production
